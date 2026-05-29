a50e3cb9a4faff2dbfcf337d3d421af90e2149b9
samadani <blerckmermberh@gmail.com>
2026-05-29 08:37:57 +0100

    fix(nav): close mobile navbar when a link is clicked

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeBtn = document.querySelector(".close-btn");
   const nav = document.querySelector(".mobile_navbar_li");
 
+  const navLinks = document.querySelectorAll(".mobile_navbar_li a")
+
   menuBtn.addEventListener("click", () => { nav.classList.add("active") });
   closeBtn.addEventListener("click", () => { nav.classList.remove("active") });
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("active");
+    });
+  });
 });
